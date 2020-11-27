89371d22bf95c594ae5047c1a39a756cf98db638
Anton Shishlachev <anton_shish@mail.ru>
2020-11-28 00:04:42 +0300

    config files realised

---
+++ src/main/java/ru/homeproject/library/config/MVCConfig.java
@@ -0,0 +1,4 @@
+package ru.homeproject.library.config;
+
+public class MVCConfig {
+}
---
+++ src/main/java/ru/homeproject/library/config/WebConfig.java
@@ -0,0 +1,32 @@
+package ru.homeproject.library.config;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    //метод содержит кофигурации. которые описывают beans
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {AppConfig.class, MVCConfig.class};
+    }
+
+    //конфигурация, в которой реализован View Resolver
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {AppConfig.class, MVCConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
